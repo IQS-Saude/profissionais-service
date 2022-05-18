3ce68fe89f4d18ce857641c3fa5629cebf7769dc
João Vitor Goncalves Pina <joaovitorpinag@hotmail.com>
2022-05-17 22:33:17 -0300

    Correcão para o objeto de requisicao do salvar tipo profissional para nao deixar string vazias
    Signed-off-by: João Vitor Goncalves Pina <joaovitorpinag@hotmail.com>

--- src/ProfissionaisService.api/Dto/SalvarTipoProfissionalRequest.cs
+++ src/ProfissionaisService.api/Dto/SalvarTipoProfissionalRequest.cs
@@ -1,3 +1,6 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ProfissionaisService.api.Dto;
 
-public record SalvarTipoProfissionalRequest(int? Id, string Descricao, List<string> Especialidades);
+public record SalvarTipoProfissionalRequest(int? Id, [Required(AllowEmptyStrings = false)] string Descricao,
+    List<string> Especialidades);
